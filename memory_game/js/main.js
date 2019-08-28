02bef4ff7f4e0969ad7b75441a33498b78b91218
Manny Santana <aresxwarlord@outlook.com>
2019-08-28 01:07:18 -0400

    added basic game logic using js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
 
-let card1 = "queen";
-let card2 = "queen";
-let card3 = "king";
-let card4 = "king";
+let cards = ["queen", "king","queen", "king"];
+let cardsInPlay = [];
 
-console.log("User flipped " + card4);
+let card1 = cards[0];
+cardsInPlay.push(card1);
+
+let card2 = cards[2];
+cardsInPlay.push(card2);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
+console.log("cards in play: " + cardsInPlay);
+
+if(cardsInPlay.length === 2){
+
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You have a match!");
+	}else { 
+		alert("Sorry try again")
+	}
+
+} else {
+
+	alert("please choose 2 cards!");
+}
