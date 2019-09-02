1aa0fe179ef18d4dea86466ae8b6bcccf78bfd0a
Manny Santana <mannyasantana@live.com>
2019-09-02 01:39:35 -0400

    added code comments for future reference

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,7 @@
 console.log("Up and running!"); //checking to see if js is working on the page
 
 
-//create
+//create card objects
 let cards = [
 {
 	rank: "queen",
@@ -21,9 +21,10 @@ let cards = [
 	cardImage: "images/king-of-diamonds.png"
 }];
 
-
+//cards chosen by user get pushed into the array
 let cardsInPlay = [];
 
+//validates matching pairs have been chosen
 function checkForMatch(){
 
 	if(cardsInPlay[0] === cardsInPlay[1]){
@@ -34,41 +35,47 @@ function checkForMatch(){
 
 }
 
+//logic for user selection of cards, passed to an event listener function
 function flipCard(){
 
 
+let cardId = this.getAttribute('data-id'); // retrieve the data-id of the card clicked on by user
+console.log("User flipped " + cards[cardId].rank); //log to console for debugging
+cardsInPlay.push(cards[cardId].rank); //add selected card to the cards in play list
 
-let cardId = this.getAttribute('data-id');
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
+//logs for debugging
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
 
+//set the src attribute of the clicked card to its corresponding card image 
 this.setAttribute('src', cards[cardId].cardImage);
 
+//logic to check for a match when player chooses 2 cards. 
 if(cardsInPlay.length === 2){
 
 	checkForMatch();
 
-} else {
+	} else {
 
 	alert("please choose 2 cards!");
 	}
 }
 
-
+//function to create the board
+//uses js to create card elements instead of html
 function createBoard(){
 	for(let i=0; i < cards.length; i++){
 
-		let cardElement = document.createElement('img');
-		cardElement.setAttribute('src',"images/back.png");
-		cardElement.setAttribute('data-id', i);
-		cardElement.addEventListener('click', flipCard);
-		const gameBoard = document.getElementById('game-board');
-		gameBoard.appendChild(cardElement);
+		let cardElement = document.createElement('img'); //create new element for each object in cards array
+		cardElement.setAttribute('src',"images/back.png");// set the image src attribute
+		cardElement.setAttribute('data-id', i);//set a data-id to identify the card clicked on by the user
+		cardElement.addEventListener('click', flipCard); // event triggered by user click to display the flipped card
+		const gameBoard = document.getElementById('game-board'); // get gamebord div 
+		gameBoard.appendChild(cardElement); // add child node to gameboard div to display imgs
 
 
 	}
 }
 
+//function call to display game
 createBoard();
